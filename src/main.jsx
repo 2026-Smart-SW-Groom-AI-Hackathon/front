416f2cdb6e9265f681d92ec09a2934b343007be5
HP657 <junlee5070@gmail.com>
2026-05-10 03:06:19 +0900

    StrictMode 싫어요

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// StrictMode는 dev에서 컴포넌트를 의도적으로 mount→unmount→mount 시키는데,
+// WebSocket이 열리는 시점에 unmount가 끼면 서버가 카메라를 release해서
+// Windows에서 카메라 핸들이 잠기는 증상이 있어 비활성화.
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
